e6cb0e6d87589e09f3ec6f8ba8c37261aaf03d20
Gnong <jinhongdev@gmail.com>
2021-04-27 02:12:19 +0900

    Adding To Dos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+const initialState = {
+  toDos: [],
+};
+
+const ADD = "add";
 
 const reducer = (state, action) => {
-  switch (action) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+  switch (action.type) {
+    case ADD:
+      return { toDos: [...state.toDos, { text: action.payload }] };
     default:
       throw new Error();
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch(INCREMENT)}>ADD</button>
-      <button onClick={() => dispatch(DECREMENT)}>MINUS</button>
+      <h1>Add to do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={index}>{toDo.text}</li>
+        ))}
+      </ul>
     </>
   );
 }
