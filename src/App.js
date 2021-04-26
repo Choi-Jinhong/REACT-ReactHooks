1d526fe2fa810307f48f559f76b3e9359dcfebfb
Gnong <jinhongdev@gmail.com>
2021-04-27 03:32:17 +0900

    Refactioring with Context

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { useReducer, useState } from "react";
-import reducer, { initialState, ADD, DEL, COMPLETE, UNCOMPLETE } from "./reducer";
+import Add from './Add';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [newToDo, setNewToDo] = useState("");
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch({ type: ADD, payload: newToDo });
-    setNewToDo("");
-  };
-  const onChange = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setNewToDo(value);
-  };
   return (
     <>
-      <h1>Add to do</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={newToDo}
-          type="text"
-          placeholder="Write to do"
-          onChange={onChange}
-        />
-      </form>
+      <Add />
       <ul>
         <h2>To Dos</h2>
         {state.toDos.map((toDo) => (
@@ -34,14 +13,16 @@ function App() {
             <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
               DELETE
             </button>
-            <button onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}>
+            <button
+              onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}
+            >
               COMPLETE
             </button>
           </li>
         ))}
       </ul>
       <ul>
-        {state.completed.length != 0 && (
+        {state.completed.length !== 0 && (
           <>
             <h2>Completed</h2>
             {state.completed.map((toDo) => (
@@ -53,7 +34,9 @@ function App() {
                   DELETE
                 </button>
                 <button
-                  onClick={() => dispatch({ type: UNCOMPLETE, payload: toDo.id })}
+                  onClick={() =>
+                    dispatch({ type: UNCOMPLETE, payload: toDo.id })
+                  }
                 >
                   UNCOMPLETE
                 </button>
